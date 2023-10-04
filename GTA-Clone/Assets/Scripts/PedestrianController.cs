9c20b21133de6a3530fcb55a113b911ab758bf00
Zeshan Rasul <73083881+pablozee@users.noreply.github.com>
2023-10-04 01:48:14 +0100

    Pedestrian FSM with walk and idle states working

--- GTA-Clone/Assets/Scripts/PedestrianController.cs
+++ GTA-Clone/Assets/Scripts/PedestrianController.cs
@@ -5,15 +5,56 @@ using UnityEngine.AI;
 
 public class PedestrianController : MonoBehaviour
 {
+    public enum State
+    {
+        Idle,
+        Walk,
+        AttackOrFlee,
+        Flee,
+        Attack
+    }
+
     [SerializeField] private bool agentActive = true;
     [SerializeField] private float walkPointRange;
     [SerializeField] private Vector3 walkPoint;
 
+    [SerializeField] private bool isDangerClose;
+    [SerializeField] private bool isAggressive;
+
+    public State currentState;
     private NavMeshAgent agent;
     private Animator animator;
     private Vector3 destination;
     private bool walkPointSet;
     private bool reachedDestination;
+    private float idleEndTime;
+
+
+    private void TransitionToState(State newState)
+    {
+        currentState = newState;
+
+        switch (newState)
+        {
+            case State.Idle:
+                idleEndTime = Time.time + Random.Range(3, 9);
+                break;
+
+            case State.Walk:
+                if (!walkPointSet) SearchWalkPoint();
+                if (walkPointSet) SetDestination(walkPoint);
+
+                break;
+
+            case State.Flee:
+                // Logic to flee, maybe setting a destination opposite to the danger source
+                break;
+
+            case State.Attack:
+                // Initialize attack, maybe lock onto the target, play an animation, etc.
+                break;
+        }
+    }
 
     private void Awake()
     {
@@ -24,7 +65,7 @@ public class PedestrianController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        TransitionToState(State.Walk);
     }
 
     // Update is called once per frame
@@ -32,10 +73,104 @@ public class PedestrianController : MonoBehaviour
     {
         if (!agentActive || agent.enabled == false) return;
 
-        if (walkPointSet) CheckPathComplete();
-        MovementAnimations();
-        Roam();
+        switch (currentState)
+        {
+            case State.Idle:
+                HandleIdleState();
+                break;
+
+            case State.Walk:
+                HandleWalkState();
+                break;
+
+            case State.AttackOrFlee:
+                HandleAttackOrFleeState();
+                break;
+
+            case State.Attack:
+                HandleAttackState();
+                break;
+
+            case State.Flee:
+                HandleFleeState();
+                break;
+        }
+
+    //    if (walkPointSet) CheckPathComplete();
+    //    MovementAnimations();
+    //    Roam();
     }
+
+    void HandleIdleState()
+    {
+        if (Time.time > idleEndTime)
+        {
+            TransitionToState(State.Walk);
+        }
+
+        if (isDangerClose)
+        {
+            TransitionToState(State.AttackOrFlee);
+        }
+    }
+
+    void HandleWalkState()
+    {
+        if (walkPointSet) CheckPathComplete();
+
+        MovementAnimations();
+
+        if (reachedDestination)
+        {
+            float chance = Random.Range(0f, 1f);
+            if (chance <= 0.2f) 
+            {
+                TransitionToState(State.Idle);
+            }
+            else
+            {
+                TransitionToState(State.Walk);
+            }
+        }
+
+        if (isDangerClose)
+        {
+            TransitionToState(State.AttackOrFlee);
+        }
+    }
+
+    void HandleAttackOrFleeState()
+    {
+        if (isAggressive || Random.Range(0f, 1f) <= 0.20f)
+        {
+            TransitionToState(State.Attack);
+        }
+        else
+        {
+            TransitionToState(State.Flee);
+        }
+    }
+
+    void HandleAttackState()
+    {
+        // Handle attack behavior.
+
+        if (!isDangerClose)
+        {
+            TransitionToState(State.Idle);
+        }
+    }
+
+    void HandleFleeState()
+    {
+        // Handle fleeing behavior. Maybe increase speed and run away from the danger source
+
+        if (!isDangerClose)
+        {
+            TransitionToState(State.Idle);
+        }
+    }
+
 
     void MovementAnimations()
     {
@@ -49,6 +184,7 @@ public class PedestrianController : MonoBehaviour
         if (!walkPointSet) SearchWalkPoint();
 
         if (walkPointSet) agent.SetDestination(walkPoint);
+
     }
 
     void SearchWalkPoint()
